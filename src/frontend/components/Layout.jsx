22da922cce159a77202c92617d76c4eceea0161c
ptiza <ptizajar@gmail.com>
2026-05-17 21:45:29 +0300

    scroll to top

--- src/frontend/components/Layout.jsx
+++ src/frontend/components/Layout.jsx
@@ -1,11 +1,13 @@
-import {Header} from "./Header";
-import {Footer} from "./Footer";
+import { Header } from "./Header";
+import { Footer } from "./Footer";
+import ScrollToTop from "./ScrollToTop";
 import React from "react";
 import { Outlet } from "react-router-dom";
 import l from "../css/.module/layout.module.css";
 export function Layout() {
   return (
     <div className={l.layout}>
+      <ScrollToTop />
       <Header />
       <main className={l.content}>
 
