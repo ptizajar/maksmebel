231768ce306859ce2e3c981792681d64971cc651
ptiza <ptizajar@gmail.com>
2026-05-25 17:17:05 +0300

    unnesessary files deleted

--- createAdmin.sql
+++
@@ -1,7 +0,0 @@
--- insert into users (email, company, password, user_name,phone,is_admin) values ('admin','','c4ca4238a0b923820dcc509a6f75849b','','',true);
-
-
--- alter table item add column removed boolean default false;
-
-alter table orders add column user_name varchar(100);
-alter table orders add column phone varchar(15);
